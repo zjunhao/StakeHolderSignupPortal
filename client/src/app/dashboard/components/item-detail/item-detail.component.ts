f953307b60e972e2f106b31a8046ce1f3bd3f4a4
Junhao Zhang <junhao.zhang@hyland.com>
2020-06-25 10:21:09 -0400

    item detail component cleanup

--- client/src/app/dashboard/components/item-detail/item-detail.component.ts
+++ client/src/app/dashboard/components/item-detail/item-detail.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DashboardService } from '../../services/dashboard.service';
 import { ActivatedRoute, Params } from '@angular/router';
-import { DetailItemModel } from '../../models/detail-item-model';
-import { CurrentUserService } from '../../services/current-user.service';
 
 @Component({
   selector: 'app-item-detail',
@@ -12,92 +9,18 @@ import { CurrentUserService } from '../../services/current-user.service';
 export class ItemDetailComponent implements OnInit {
 
   editMode: boolean = false;
-  itemDetail: DetailItemModel = new DetailItemModel();
-  signupResultMessage: string = '';
 
   constructor(
-    private dashBoardService: DashboardService,
-    private currentUserService: CurrentUserService,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.refreshItemDetail();
-  }
-
-  refreshItemDetail() {
-    // request example like .../:id/?editMode=false
-
-    // extract item id from request parameter
-    this.route.params.forEach((params: Params) => {
-      if (params['id'] !== undefined) {
-        const id = params['id'];
-        this.dashBoardService.getSprintReview(id).subscribe(itemDetail => {
-          this.itemDetail = itemDetail;
-        });
-      } 
-    });
-
     // extract editMode bool from request query parameter 
     this.route.queryParams.forEach((params: Params) => {
       if (params['editMode'] !== undefined) {
         this.editMode = params['editMode'] === 'true';
       }
+      // this.editMode=true;
     })
   }
-
-  //-----------------------for normal user-----------------------------------------
-  attendeeSignUp() {
-    this.dashBoardService.attendeeSignUp(this.itemDetail._id, this.currentUserService.getId()).subscribe(res => {
-      if (res.message) {
-        this.signupResultMessage = res.message;
-      }
-    });
-  }
-
-  //-----------------------for administrator---------------------------------------
-  onTitleUpdate() {
-    const updateId = this.itemDetail._id;
-    const updateBody = {"title" : this.itemDetail.title};
-    this.dashBoardService.updateSprintReview(updateId, updateBody).subscribe(()=>{
-      this.refreshItemDetail();
-    });
-
-  }
-  onOrganzerUpdate() {
-    const updateId = this.itemDetail._id;
-    const updateBody = {"organizer" : this.itemDetail.organizer};
-    this.dashBoardService.updateSprintReview(updateId, updateBody).subscribe(()=>{
-      this.refreshItemDetail();
-    });
-  }
-  onStartTimeUpdate() {
-    const updateId = this.itemDetail._id;
-    const updateBody = {"startTime" : this.itemDetail.startTime};
-    this.dashBoardService.updateSprintReview(updateId, updateBody).subscribe(()=>{
-      this.refreshItemDetail();
-    });
-  }
-  onEndTimeUpdate() {
-    const updateId = this.itemDetail._id;
-    const updateBody = {"endTime" : this.itemDetail.endTime};
-    this.dashBoardService.updateSprintReview(updateId, updateBody).subscribe(()=>{
-      this.refreshItemDetail();
-    });
-  }
-  onDescriptionUpdate() {
-    const updateId = this.itemDetail._id;
-    const updateBody = {"description" : this.itemDetail.description};
-    this.dashBoardService.updateSprintReview(updateId, updateBody).subscribe(()=>{
-      this.refreshItemDetail();
-    });
-  }
-  onMeetingLinkUpdate() {
-    const updateId = this.itemDetail._id;
-    const updateBody = {"meetingLink" : this.itemDetail.meetingLink};
-    this.dashBoardService.updateSprintReview(updateId, updateBody).subscribe(()=>{
-      this.refreshItemDetail();
-    });
-  }
-
 }
